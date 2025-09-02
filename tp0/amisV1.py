e6ab6478f85b4aabb1796c61cf67189f6817c49b
fdb <fdb@mail.com>
2025-09-02 12:29:10 +0200

    init2

--- tp0/amisV1.py
+++ tp0/amisV1.py
@@ -2,3 +2,21 @@
 
 if __name__ == '__main__':
     print('coucou3')
+    #max = int(input("entrez max : "))
+    max = 500
+    for n in range(1,max+1) :
+        sn = 1
+        i = 2
+        while i <= n//2:
+            if n % i == 0:
+                sn = sn + i
+            i = i + 1
+        for m in range(1,max+1) :
+            sm = 1
+            i = 2
+            while i <= m//2:
+                if m % i == 0:
+                    sm = sm + i
+                i = i + 1
+            if sm == n and sn == m :
+                print(f"{n} et {m} sont amis")
---
+++ tp0/amisV2.py
@@ -0,0 +1,21 @@
+
+
+if __name__ == '__main__':
+    print('coucou3')
+    #max = int(input("entrez max : "))
+    max = 500
+    for n in range(1,max+1) :
+        sn = 1
+        i = 2
+        while i <= n//2:
+            if n % i == 0:
+                sn = sn + i
+            i = i + 1
+        sm = 1
+        i = 2
+        while i <= sn//2:
+            if sn % i == 0:
+                sm = sm + i
+            i = i + 1
+        if sm == n :
+            print(f"{n} et {sn} sont amis")
