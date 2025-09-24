5623151f84eea26a173d086fa5b23d8d7a5a5e04
fdb <fdb@mail.com>
2025-09-24 17:57:51 +0200

    noeuds RLC

--- circuitRLC/model.py
+++ circuitRLC/model.py
@@ -1,5 +1,8 @@
 import math
 
+from shiboken6.Shiboken import Object
+
+
 class Noeud :
     def __init__(self,px : float,py : float):
         self._px = px
@@ -29,14 +32,47 @@ class Noeud :
         dy = self.py - y
         return math.sqrt(dx*dx + dy*dy)
 
-    def distanceNoeud(self,n2 : 'Noeud') -> float:
+    def distance_noeud(self,n2 : 'Noeud') -> float:
         return self.distance(n2.px,n2.py)
 
 def test1() :
     n1 = Noeud(0,0)
     print(f"distance : {n1.distance(1,1)}")
     n2 = Noeud(2,3)
-    print(f"distance {n1}-{n2} : {n1.distanceNoeud(n2)}")
+    print(f"distance {n1}-{n2} : {n1.distance_noeud(n2)}")
+
+class Composant :
+    pass
+
+class Generateur(Composant) :
+    pass
+
+class Condensateur(Composant) :
+    def __init__(self,capacite : float):
+        if capacite < 0 :
+            raise Exception("capacite négative : n'importe quoi !!")
+        self._capacite = capacite
+
+    @property
+    def capacite(self):
+        return self._capacite
+
+    @capacite.setter
+    def capacite(self,capacite : float):
+        if capacite < 0 :
+            raise Exception("capacite négative : n'importe quoi !!")
+        self._capacite = capacite
+
+    def __str__(self):
+        return f"[Condensateur : {self.capacite*1000000} microF]"
+
+def test2() :
+    g1 = Generateur()
+    print(isinstance(g1,Composant))
+    o1 = Object()
+    print(isinstance(o1,Composant))
+
 
 if __name__ == '__main__':
-    test1()
+    # test1()
+    test2()
